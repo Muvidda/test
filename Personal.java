7fa5c12881532908a2ea70b0a8ff93350997a145
Muriel Nartey <murielnarety3@gmail.com>
2013-09-13 11:11:32 +0000

    personal details

---
+++ Personal.java
@@ -0,0 +1,11 @@
+public class Personal{
+
+	public static void main(String[] args){
+		int age=22;
+		String name="Muriel";
+		double height=1.75;
+
+		System.out.println("Hello "+name+"."+" So you are "+age+" years old and your height is "+height+" metres.");
+	}
+
+} 
